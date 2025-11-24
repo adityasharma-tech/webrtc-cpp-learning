b9fe3092e1f02fad27677fb56764f6e1351b450c
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-11-24 18:49:45 +0530

    feat: simple webrtc java code added and /webrtc/include/sdk added to the cpp lib

--- app/src/main/cpp/prodwebrtc.cpp
+++ app/src/main/cpp/prodwebrtc.cpp
@@ -1,13 +1,14 @@
 #include <jni.h>
-//#include "webrtc/include/modules/audio_device/include/audio_device.h"
-//#include "webrtc/include/modules/audio_device/include/audio_device_factory.h"
+
+#define WEBRTC_POSIX 1
+#define WEBRTC_ANDROID 1
+#define WEBRTC_ARCH_ARM64 1
+
+#include "webrtc/include/rtc_base/checks.h"
+#include "webrtc/include/rtc_base/ssl_adapter.h"
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_tech_adityasharma_prodwebrtc_ProductionService_nativeInit(JNIEnv *env, jobject thiz) {
-    // TODO: implement nativeInit()
-}
 
-//class CustomAudioDeviceModule : public webrtc::AudioDeviceModule {
-//
-//};
+}
--- app/src/main/java/tech/adityasharma/prodwebrtc/MainActivity.java
+++ app/src/main/java/tech/adityasharma/prodwebrtc/MainActivity.java
@@ -9,7 +9,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.DataChannel;
+import org.webrtc.DefaultVideoDecoderFactory;
+import org.webrtc.DefaultVideoEncoderFactory;
+import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
+import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PeerConnectionFactory peerConnectionFactory;
+    private PeerConnection peerConnection;
+
+    private EglBase eglBase;
+
+    private SurfaceViewRenderer localView;
+    private VideoCapturer videoCapturer;
+    private VideoSource videoSource;
+    private VideoTrack localVideoTrack;
+    private AudioSource audioSource;
+    private AudioTrack localAudioTrack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +56,110 @@ public class MainActivity extends AppCompatActivity {
 
         Intent productionServiceIntent = new Intent(this, ProductionService.class);
         startService(productionServiceIntent);
+    }
+
+    private void initializeWebRTC(){
+        PeerConnectionFactory.InitializationOptions initializationOptions = PeerConnectionFactory.InitializationOptions.builder(this)
+                .setEnableInternalTracer(true)
+                .createInitializationOptions();
+        PeerConnectionFactory.initialize(initializationOptions);
+        eglBase = EglBase.create();
+        PeerConnectionFactory.Options factoryOptions = new PeerConnectionFactory.Options();
+
+        peerConnectionFactory = PeerConnectionFactory.builder()
+                .setOptions(factoryOptions)
+                .setVideoEncoderFactory(new DefaultVideoEncoderFactory(eglBase.getEglBaseContext(), true, true))
+                .setVideoDecoderFactory(new DefaultVideoDecoderFactory(eglBase.getEglBaseContext()))
+                .createPeerConnectionFactory();
+    }
+
+    private VideoCapturer createCameraCapturer() {
+        Camera2Enumerator enumerator = new Camera2Enumerator(this);
+        for (String deviceName : enumerator.getDeviceNames()) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer capturer = enumerator.createCapturer(deviceName, null);
+                if (capturer != null) return capturer;
+            }
+        }
+        return null;
+    }
+
+    private void createLocalMediaTracks(){
+        videoCapturer = createCameraCapturer();
+        assert videoCapturer != null;
+        videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
+        SurfaceTextureHelper surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBase.getEglBaseContext());
+
+        videoCapturer.initialize(surfaceTextureHelper, this, videoSource.getCapturerObserver());
+        localVideoTrack = peerConnectionFactory.createVideoTrack("LOCAL_VIDEO", videoSource);
+
+        audioSource = peerConnectionFactory.createAudioSource(new MediaConstraints());
+        localAudioTrack = peerConnectionFactory.createAudioTrack("LOCAL_AUDIO", audioSource);
+    }
+
+    private void startLocalVideo() {
+        videoCapturer.startCapture(1280, 720, 30);
+        localVideoTrack.addSink(localView);
+    }
+
+    private void createPeerConnection(){
+        PeerConnection.RTCConfiguration rtcConfiguration = new PeerConnection.RTCConfiguration(new ArrayList<>());
+
+        peerConnection = peerConnectionFactory.createPeerConnection(rtcConfiguration, new PeerConnection.Observer() {
+            @Override
+            public void onSignalingChange(PeerConnection.SignalingState newState) {
+
+            }
+
+            @Override
+            public void onIceConnectionChange(PeerConnection.IceConnectionState newState) {
+
+            }
+
+            @Override
+            public void onIceConnectionReceivingChange(boolean receiving) {
+
+            }
+
+            @Override
+            public void onIceGatheringChange(PeerConnection.IceGatheringState newState) {
+
+            }
+
+            @Override
+            public void onIceCandidate(IceCandidate candidate) {
+
+            }
+
+            @Override
+            public void onIceCandidatesRemoved(IceCandidate[] candidates) {
+
+            }
+
+            @Override
+            public void onAddStream(MediaStream stream) {
+
+            }
+
+            @Override
+            public void onRemoveStream(MediaStream stream) {
+
+            }
+
+            @Override
+            public void onDataChannel(DataChannel dataChannel) {
+
+            }
+
+            @Override
+            public void onRenegotiationNeeded() {
+
+            }
+        });
+
+        MediaStream mediaStream = peerConnectionFactory.createLocalMediaStream("LOCAL_STREAM");
+        mediaStream.addTrack(localVideoTrack);
+        mediaStream.addTrack(localAudioTrack);
+        peerConnection.addStream(mediaStream);
     }
 }
